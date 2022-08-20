fd08044cacfead7af2336bea93c249ede6ab9f31
Ashish <93777426+shedageashish24@users.noreply.github.com>
2022-08-20 23:48:19 +0530

    Deploy

---
+++ src/SortingAlgorithms/BubbleSort.js
@@ -0,0 +1,49 @@
+export function getBubbleSortAnimations(array){
+    let animations = [];
+    let auxillaryArray = array.slice();
+    bubbleSort(auxillaryArray , animations);
+    const javaScriptSortedArray = array.slice().sort((a, b) => a - b);
+    console.log("sort works correctly? ",arraysAreEqual(javaScriptSortedArray, auxillaryArray));
+    array = auxillaryArray;
+    return [animations, array];
+}
+
+
+function bubbleSort(auxillaryArray , animations){
+    const N = auxillaryArray.length;
+    for(let i  = 0 ; i < N-1 ; i++){
+        let swapped = false;
+        for(let j = 0 ; j < N-1-i ; j++){
+            animations.push(["comparision1", i , i+1]);
+            animations.push(["comparision2", i , i+1]);
+            if(auxillaryArray[i] > auxillaryArray[i+1]){
+                swapped = true;
+                animations.push(["swap", i , auxillaryArray[i+1]]);
+                animations.push(["swap", i+1 , auxillaryArray[i]]);
+                swap(auxillaryArray , i , i+1);
+            }
+        }
+        if(swapped === false) break;
+    }
+}
+
+
+function swap(auxillaryArray , firstIndex , secondIndex){
+    let temp = auxillaryArray[firstIndex];
+    auxillaryArray[firstIndex] = auxillaryArray[secondIndex];
+    auxillaryArray[secondIndex] = temp;
+
+}
+
+
+function arraysAreEqual(firstArray, secondArray) {
+    if (firstArray.length !== secondArray.length) {
+        return false;
+    }
+    for (let i = 0; i < firstArray.length; i++) {
+      if (firstArray[i] !== secondArray[i]) {
+        return false;
+      }
+    }
+    return true;
+}
